6774f796424ef692bb7a9fd0b4a9ae697e3d3a10
tbostwick <tbostwick56@gmail.com>
2026-03-16 14:51:34 -0600

    added analyses for pca and kinship, will remove zoo for the next iteration, was used to check representation of the kinship values

--- scripts/LEPA_Manuscript1.R
+++ scripts/LEPA_Manuscript1.R
@@ -51,7 +51,7 @@ library(dplyr)
   #/Users/tylerbostwick/Documents/Masters_Work/Analyses/1_Data/1_Working_Files/joint_call.LAO03M.20251202.vcf.gz \
   #-O z -o /Users/tylerbostwick/Documents/Masters_Work/Analyses/1_Data/1_Working_Files/joint_call_autosomes.vcf.gz
 
-####reading creating plink files from the joint_call VCF file####
+####creating plink files from the joint_call VCF file####
 system("./plink2 --vcf joint_call_autosomes.vcf.gz --keep-allele-order --allow-extra-chr --vcf-min-dp 10 --max-alleles 2 --chr-set 18 --make-bed --out SNP_AllChrom_AllInd")
       #89 individuals 107697881 variants remain after filter for depth and biallelic
 
@@ -83,14 +83,14 @@ system("./plink2 --bfile SNP_AllInd_AllChrom_biallelic_chrfix --chr-set 17 --set
 
 ###subset data -- remove mountain lion; all populations and origins get filtered together
 system("./plink --bfile SNP_AllInd_AllChrom_biallelic_uniqueID --keep pop_subset_ocelot.txt --chr-set 17 --make-bed --out LEPA_biallelic_dp10")
-    #Total genotyping rate in remaining samples is 0.694948; 107697881 variants and 86 samples pass filters and QC.
+    #Total genotyping rate in remaining samples is 0.694513; 107697881 variants and 85 samples pass filters and QC.
     #at this point, the only filters applied are coverage depth and biallelic
 
 ####SNP filtering to create standard data set for all LEPA####
 #apply filters -- maf, miss, hwe> base, other adjustments can follow
 ###LEPA
 system("./plink --bfile LEPA_biallelic_dp10 --chr-set 17 --keep-allele-order --maf 0.05 --geno 0.1 --hwe 1e-6 --make-bed --out LEPA_standard_final")
-      #57793 variants and 86 samples pass filters and QC.
+      #61480 variants and 85 samples pass filters and QC
 
 ####Export full lepa standard vcf
 system("./plink2 --bfile LEPA_standard_final --chr-set 17 --export vcf-4.2 bgz --out LEPA_standard_final")
@@ -104,16 +104,118 @@ system("./plink --bfile LEPA_standard_final --keep zoo_subset.txt --chr-set 17 -
 #LEPA
 system("./plink --bfile LEPA_standard_final --chr-set 17 --keep-allele-order --indep 50 5 2 --out LEPA_LDpruned_0.5_out") #makes an out and in files of SNps to keep and SNPs to remove
 system("./plink --bfile LEPA_standard_final --extract LEPA_LDpruned_0.5_out.prune.in --chr-set 17 --make-bed --out LEPA_LDpruned_05") #extract SNPs and create new files
-    #Total genotyping rate is 0.925572; 13161 variants and 86 samples pass filters and QC.
+    #Total genotyping rate is 0.925572; 13965 variants and 85 samples pass filters and QC.
 #write vcf
 system("./plink2 --bfile LEPA_LDpruned_05 --chr-set 17 --export vcf-4.2 bgz --out LEPA_LDpruned_05")
 
 #ROH filters; no MAF, miss 90, biallelic, coverage depth
 #LEPA
 system("./plink --bfile LEPA_biallelic_dp10 --chr-set 17 --keep-allele-order --geno 0.1 --hwe 1e-6 --make-bed --out LEPA_roh_filter")
-    #292296 variants and 86 samples pass filters and QC.
+    #309052 variants and 85 samples pass filters and QC.
 
 
 ##############################################################
 ####Data Analysis
-####PCA Analysis####
+####PCA Analysis -- Done!####
+##PCA on Wild individuals --- done!
+#reading in data
+system("./plink --bfile Wild_Standard_postfilt_subset --pca --chr-set 17 --out pca_wild") #run the pca code, specified the number of chromosomes
+pca.data.wild <- read.table("pca_wild.eigenvec", header=FALSE) #load pca results from the eigenvec file
+colnames(pca.data.wild)[2] <- "ID" #rename second column to allow for appending pop assignment later
+eigenvalues <- read.table("pca_wild.eigenval", header=FALSE)$V1 #read in eigenvalues to calc variance
+#calc variance
+total_variance <- sum(eigenvalues)
+pc1_variance <- round((eigenvalues[1] / total_variance) * 100, 2)
+pc2_variance <- round((eigenvalues[2] / total_variance) * 100, 2)
+#adding population information
+wild_origins <- read.csv("wild_origins.csv", header = TRUE) #read in origins file
+pca.data.wild.origins <- left_join(pca.data.wild, wild_origins, by = "ID") #append origin data to pca data
+pca.data.wild.origins <- pca.data.wild.origins %>%
+  mutate(ID = gsub("-.*", "", ID))
+#plotting
+wild_pca <- ggplot(pca.data.wild.origins, aes(x=V3,y=V4)) +  #plot with individual ID's and by origin
+  geom_point(aes(shape = Pop, color = Pop), size = 3) +
+  geom_rect(data = subset(pca.data.wild.origins, ID %in% c("E35M", "LO01F")),
+            aes(xmin = min(V3) - 0.02, xmax = max(V3) + 0.04,
+                ymin = min(V4) - 0.02, ymax = max(V4) + 0.04),
+            fill = NA, color = "black", linewidth = 1, linetype = "dashed") +
+  geom_text(data = subset(pca.data.wild.origins, ID %in% c("LO03M")), #in correct position
+            aes(label=ID), vjust=-0.6, hjust=-0.1, size=4, color = "black") + 
+  geom_text(data = subset(pca.data.wild.origins, ID %in% c("OM331")), #in correct position
+            aes(label=ID), vjust=-0.6, hjust=-0.1, size=4, color = "black") + 
+  geom_text(data = subset(pca.data.wild.origins, ID %in% c("E35M")), #in correct position
+            aes(label=ID), vjust=-0.6, hjust=-0.1, size=4, color = "black") + 
+  geom_text(data = subset(pca.data.wild.origins, ID %in% c("LO01F")), #in correct position
+            aes(label=ID), vjust=-1, hjust=0.02, size=4, color = "black") + 
+  geom_text(data = subset(pca.data.wild.origins, ID %in% c("E29M")), #in correct position
+            aes(label=ID), vjust=-0.5, hjust=-0.2, size=4, color = "black") + 
+  geom_text(data = subset(pca.data.wild.origins, ID %in% c("E32M")),  #in correct position
+            aes(label=ID), vjust=-0.4, hjust=-0.2, size=4, color = "black") + 
+  scale_color_manual(name = "Origin", values = c("Refuge" = "#01004c", "Ranch" = "orchid")) +
+  scale_shape_manual(name = "Origin", values = c("Refuge" = 19, "Ranch" = 17)) +
+  labs(x = paste0("PC1 (", pc1_variance, "%)"),
+       y = paste0("PC2 (", pc2_variance, "%)")) +
+  theme_minimal() +
+  theme(
+    legend.title = element_text(size = 14),
+    legend.text = element_text(size = 12),
+    axis.title.x = element_text(size = 14),
+    axis.title.y = element_text(size = 14),
+    axis.text = element_text(size = 12)
+  )
+wild_pca
+ggsave("wild_pca.png", dpi = 300)
+
+####Kinship -- mostly done, needs accuracy check####
+##king-robust kingship estimator for WILD individuals -- come back and recheck to accuracy? why all related?
+system("./plink2 --bfile Wild_Standard_postfilt_subset --make-king-table --allow-extra-chr --chr-set 17 --out wild_KING_manu")
+king.wild.matrix <- read.table("wild_KING_manu.kin0", header = FALSE) #make king table into object
+colnames(king.wild.matrix) <- c("#FID1", "IID1", "FID2", "IID2", "NSNP", "HETHET", "IBS0", "KINSHIP") #add column header
+#remove "-x" from the individual id's
+king.wild.matrix <- king.wild.matrix %>%
+  mutate(IID1 = gsub("-.*", "", IID1),
+         IID2 = gsub("-.*", "", IID2))
+#make csv from the .kin0 file
+write.csv(king.wild.matrix, "wild_pairwise_kinship_manu.csv")
+
+#heat map of kinship for wild individuals -- binned
+ggplot(data = king.wild.matrix, aes(x=IID1, y=IID2, fill = KINSHIP)) +
+  geom_tile(color="white") +
+  scale_fill_stepsn(name = "Kinship", breaks = c(0, 0.04, 0.1, 0.2),
+                    limit = c(0, 0.3),
+                    labels = c("Unrelated", "3rd Degree", "2nd Degree", "1st Degree"),
+                    aesthetics = "fill",
+                    space = "Lab",
+                    colors = c("grey100", "gold1", "orangered", "firebrick"))+
+  labs(x="Individuals", y="Individuals")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+##king-robust kingship estimator for ZOO individuals -- remove later, using to check
+##kinship definitely overrepresented with the new data, or settings are off for some reason
+system("./plink2 --bfile Zoo_Standard_postfilt_subset --make-king-table --allow-extra-chr --chr-set 17 --out zoo_KING_manu")
+zoo.king.matrix <- read.table("zoo_KING_manu.kin0", header = FALSE) #make king table into object
+colnames(zoo.king.matrix) <- c("#FID1", "IID1", "FID2", "IID2", "NSNP", "HETHET", "IBS0", "KINSHIP") #add column header
+#remove "-x" from the individual id's
+zoo.king.matrix <- zoo.king.matrix %>%
+  mutate(IID1 = gsub("-.*", "", IID1),
+         IID2 = gsub("-.*", "", IID2))
+#make csv from the .kin0 file
+write.csv(zoo.king.matrix, "zoo_pairwise_kinship_manu.csv")
+
+#heat map of kinship for zoo individuals -- binned
+zoo.king.matrix <- read.csv("zoo_pairwise_kinship_manu.csv", header = TRUE) #read in data from previous step
+zoo.king.matrix$IID1 <- as.factor(zoo.king.matrix$IID1) #changing the id names to factors to plot correctly
+zoo.king.matrix$IID2 <- as.factor(zoo.king.matrix$IID2)
+ggplot(data = zoo.king.matrix, aes(x=IID1, y=IID2, fill = KINSHIP)) +
+  geom_tile(color="white") +
+  scale_fill_stepsn(name = "Kinship", breaks = c(0, 0.04, 0.1, 0.2),
+                    limit = c(0, 0.3),
+                    labels = c("Unrelated", "3rd Degree", "2nd Degree", "1st Degree"),
+                    aesthetics = "fill",
+                    space = "Lab",
+                    colors = c("grey100", "gold1", "orangered", "firebrick"))+
+  labs(x="Individuals", y="Individuals")+
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
